42305da61ca044854b74dfd5d14c2b83cef4382e
Tobias Pfeil <tobias.pfeil02@gmail.com>
2025-01-25 15:27:49 +0100

    Integrate timer into game board and add missing logic. Handle difficulty switch (change timer dur & word length)

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import {Button} from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import { fetchWord, fetchWordMeaning } from '@/api/wordService.ts'
 import { onMounted, ref } from 'vue'
 import KeyBoard from '@/components/KeyBoard.vue'
 import { useGameStore } from '@/stores/game.ts'
 import Hangman from '@/components/Hangman.vue'
+import TimerCard from '@/components/TimerCard.vue'
+import { containsAllChars } from '@/lib/utils.ts'
 
 const word = ref()
 const game = useGameStore()
@@ -13,6 +15,7 @@ function initWord() {
   fetchWord().then((res) => {
     word.value = res
     game.setWord(word.value)
+    game.startTimer()
   })
   fetchWordMeaning(word.value).then((res) => {
     game.setWordMeaning(res)
@@ -24,11 +27,6 @@ function resetGame() {
   initWord()
 }
 
-function containsAllChars(array: string[], str: string) {
-  const uniqueChars = new Set(str)
-  return Array.from(uniqueChars).every((char) => array.includes(char))
-}
-
 onMounted(() => {
   initWord()
 })
@@ -36,36 +34,50 @@ onMounted(() => {
 
 <template>
   <div class="h-full flex flex-col justify-center items-center gap-4">
+    <TimerCard :initial-time="game.timer" />
     <div class="w-full h-[50vh]">
-  <Hangman />
+      <Hangman />
     </div>
-  <div v-if="containsAllChars(game.correctLetters, game.word)">
-    <h2>Success</h2>
-    <p>You guessed the correct word: {{ game.word }} !</p>
-    <p v-if="game.wordMeaning !== 'Lacking a definition or value.'">
-      {{ game.word }} means {{ game.wordMeaning }}
-    </p>
-    <button class="btn-primary" @click="resetGame()">Try Again</button>
-  </div>
-  <div v-else-if="game.wrongAttempts < game.maxAttempts" class="flex flex-col justify-center items-center gap-4">
-    <h1 v-if="word">
-      <span v-for="(char, index) in word.split('')" v-bind:key="char + index" class="text-4xl">{{
+    <div v-if="!game.gameOver" class="flex flex-col justify-center items-center gap-4">
+      <h1 v-if="word">
+        <span v-for="(char, index) in word.split('')" v-bind:key="char + index" class="text-4xl">{{
           game.correctLetters.includes(char) ? char : '_ '
         }}</span>
-    </h1>
-    <KeyBoard />
-  </div>
+      </h1>
+      <KeyBoard />
+    </div>
+    <div v-else>
+      <div
+        v-if="containsAllChars(game.correctLetters, game.word)"
+        class="flex flex-col justify-center items-center gap-2"
+      >
+        <h2 class="text-2xl">Success</h2>
+        <p class="text-lg">You guessed the correct word: {{ game.word }} !</p>
+        <p v-if="game.wordMeaning !== 'Lacking a definition or value.'">
+          {{ game.word }} means {{ game.wordMeaning }}
+        </p>
+        <Button class="btn-primary" @click="resetGame()">Try Again</Button>
+      </div>
 
-  <div v-else class="text-center flex flex-col justify-center items-center gap-2">
-    <h2 class="text-2xl">Game Over</h2>
-    <p class="text-lg">The word we were looking for was: {{ game.word }}</p>
-    <p v-if="game.wordMeaning !== 'Lacking a definition or value.'" class="text-lg">
-      {{ game.word }} means {{ game.wordMeaning }}
-    </p>
-    <Button class="btn-primary" @click="resetGame()">Try Again</Button>
-  </div>
+      <div
+        v-else-if="game.wrongAttempts >= game.maxAttempts"
+        class="text-center flex flex-col justify-center items-center gap-2"
+      >
+        <h2 class="text-2xl">Game Over</h2>
+        <p class="text-lg">The word we were looking for was: {{ game.word }}</p>
+        <p v-if="game.wordMeaning !== 'Lacking a definition or value.'" class="text-lg">
+          {{ game.word }} means {{ game.wordMeaning }}
+        </p>
+        <Button class="btn-primary" @click="resetGame()">Try Again</Button>
+      </div>
+      <div v-else class="text-center flex flex-col justify-center items-center gap-2">
+        <h2 class="text-2xl">Game Over</h2>
+        <p class="text-lg">You ran out of time!</p>
+        <p class="text-lg">The word we were looking for was: {{ game.word }}</p>
+        <Button class="btn-primary" @click="resetGame()">Try Again</Button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/TimerCard.vue
+++ src/components/TimerCard.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, watch, onUnmounted } from 'vue'
+import { useGameStore } from '@/stores/game.ts'
+import { Badge } from '@/components/ui/badge'
 
 const props = defineProps({
   initialTime: {
@@ -8,14 +10,78 @@ const props = defineProps({
   },
 })
 
+const gameStore = useGameStore()
 const time = ref(props.initialTime)
+const timerInterval = ref<number | null>(null)
+
+const formattedTime = computed(() => {
+  const minutes = Math.floor(time.value / 60)
+  const seconds = time.value % 60
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+})
+
+const startTimer = () => {
+  if (timerInterval.value !== null) {
+    clearInterval(timerInterval.value)
+  } else {
+    timerInterval.value = window.setInterval(() => {
+      if (time.value > 0) {
+        time.value--
+      } else {
+        gameStore.triggerGameOver()
+        resetTimer()
+      }
+    }, 1000)
+  }
+}
+
+const pauseTimer = () => {
+  gameStore.pauseTimer()
+  if (timerInterval.value !== null) {
+    clearInterval(timerInterval.value)
+    timerInterval.value = null
+  }
+}
+
+const resetTimer = () => {
+  pauseTimer()
+  time.value = props.initialTime
+}
+
+// Cleanup on unmount
+onUnmounted(() => {
+  if (timerInterval.value !== null) {
+    clearInterval(timerInterval.value)
+  }
+})
+
+watch(
+  () => gameStore.isRunning,
+  (isRunning) => {
+    if (isRunning) {
+      startTimer()
+    } else {
+      pauseTimer()
+    }
+  },
+)
+watch(
+  () => gameStore.gameOver,
+  (gameOver) => {
+    if (gameOver) {
+      resetTimer()
+    }
+  },
+)
 </script>
 
-})
 <template>
-  <div class="timer">
-    <p>{{ time }}</p>
+  <div class="mx-auto my-0 w-1/5 h-16">
+    <Badge
+      class="w-full h-full text-center text-2xl justify-center"
+      :variant="time > 10 ? 'default' : 'destructive'"
+    >
+      {{ formattedTime }}
+    </Badge>
   </div>
 </template>
-
-<style scoped></style>
